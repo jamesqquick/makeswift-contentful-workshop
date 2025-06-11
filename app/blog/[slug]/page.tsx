8d8b0356e1f9c0e57d9d5aa4fdcdd91eece35250
jamesqquick <james.q.quick@gmail.com>
2025-06-11 12:37:23 -0400

    updating defualt imports

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,8 +1,14 @@
 import { notFound } from 'next/navigation'
 
-import { getBlog } from '@/lib/contentful/fetchers'
-import { formatBlog } from '@/lib/contentful/utils'
-import { BlogPostContent } from '@/vibes/soul/sections/blog-post-content'
+import { MakeswiftComponent } from '@makeswift/runtime/next'
+import { getSiteVersion } from '@makeswift/runtime/next/server'
+
+import { BLOG_POST_EMBEDDED_COMPONENT_ID } from '@/components/BlogPostCustomizable/BlogPost.makeswift'
+import { GetBlogsDocument } from '@/generated/contentful'
+import { client } from '@/lib/contentful/client'
+import { getAllBlogs } from '@/lib/contentful/fetchers'
+import { ContentfulProvider } from '@/lib/contentful/provider'
+import { client as MakeswiftClient } from '@/lib/makeswift/client'
 
 export async function generateStaticParams() {
   //TODO: implement this function to generate static params for all blog posts
