21bdfc9f967a45fc6f3238046ea0a23c8bd5061c
Drew Pledger <drew@makeswift.com>
2025-07-01 17:52:53 -0400

    fix: overall improvements to implementation

--- lib/contentful/utils.ts
+++ lib/contentful/utils.ts
@@ -1,27 +1,37 @@
 import { documentToHtmlString } from '@contentful/rich-text-html-renderer'
+import { Document } from '@contentful/rich-text-types'
 
-import { BlogPost } from '@/vibes/soul/primitives/blog-post-card'
+import { GetBlogsQuery } from '@/generated/contentful'
+import { BlogPost as FormattedBlogPost } from '@/vibes/soul/primitives/blog-post-card'
 
-import { QueriedBlogPost } from './fetchers'
+export type BlogPostFromQuery = NonNullable<
+  NonNullable<GetBlogsQuery['blogPostCollection']>['items'][0]
+>
 
-export const formatBlogs = (blogs: QueriedBlogPost[], includeBody: boolean = true): BlogPost[] => {
+export const formatBlogs = (
+  blogs: BlogPostFromQuery[],
+  includeBody: boolean = true
+): FormattedBlogPost[] => {
   return blogs.map(blog => formatBlog(blog, includeBody))
 }
 
-export const formatBlog = (blog: QueriedBlogPost, includeBody: boolean = true): BlogPost => {
-  if (!blog.title || !blog.publishDate || !blog.body || !blog.banner?.url || !blog.body?.json) {
-    throw new Error('Blog post is missing required fields: title, publishDate, or body')
+export const formatBlog = (
+  blog: BlogPostFromQuery,
+  includeBody: boolean = true
+): FormattedBlogPost => {
+  if (!blog.title || !blog.feedDate || !blog.body || !blog.banner?.url || !blog.body?.json) {
+    throw new Error('Blog post is missing required fields: title, feedDate, or body')
   }
   return {
     title: blog.title,
-    date: new Date(blog.publishDate).toLocaleDateString('en-US', {
+    date: new Date(blog.feedDate).toLocaleDateString('en-US', {
       month: 'long',
       day: 'numeric',
       year: 'numeric',
     }),
-    content: includeBody ? documentToHtmlString(blog.body.json) : blog.description,
+    content: includeBody ? documentToHtmlString(blog.body.json as Document) : blog.description,
     image: blog.banner ? { src: blog.banner.url, alt: blog.title } : null,
-    author: blog.author,
+    author: blog.author?.name,
     href: `/blog/${blog.slug}`,
   }
 }
